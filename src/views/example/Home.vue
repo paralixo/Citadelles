9c007067c7ce2a9d6e4497a6fe1f8007e7c4a256
paralix0 <florian.lafuente@gmail.com>
2020-01-06 11:58:32 +0100

    test_branche3

--- src/views/example/Home.vue
+++ src/views/example/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../../assets/logo.png">bison3
+    <img alt="Vue logo" src="../../assets/logo.png">bison4
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
