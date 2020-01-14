15bbf51d54615f30426e3fa77d0f9d9605a87937
jesuisuncoucous <34130023+jesuisuncoucous@users.noreply.github.com>
2020-01-14 10:21:35 +0100

    coin & board

--- src/components/Playground.vue
+++ src/components/Playground.vue
@@ -1,26 +1,16 @@
 <template>
     <div>
         <slot></slot>
-        <p class="count">5</p>
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator"
 @Component
-export default class Card extends Vue {
-    @Prop() public src!: string
+export default class Playground extends Vue {
 }
 </script>
 
 <style lang="css" scoped>
-
-.count{
-    color: red;
-    position: absolute;
-    bottom:0%;
-    left:40%;
-    font-size:34px
-}
 
 </style>
--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,5 +1,6 @@
 <template>
 <div>
+      <Playground><img class="playground" src="../assets/images/rectangle-.png-5.png" alt=""></Playground>
       <div class="hand">
         <Card class="card" ><img src="../assets/cards/roi.jpg" alt=""></Card>
         <Card class="card" ><img src="../assets/cards/roi.jpg" alt=""></Card>
@@ -7,8 +8,8 @@
         <Card class="card" ><img src="../assets/cards/roi.jpg" alt=""></Card>
         <Card class="card" ><img src="../assets/cards/roi.jpg" alt=""></Card>
       </div>
-      <div>
-        <Coin><img src="../assets/images/piece.jpg" alt=""></Coin>
+      <div class="coin">
+        <Coin><img class="coinSprite" src="../assets/images/piece.jpg" alt=""></Coin>
       </div>
   </div>
 </template>
@@ -17,9 +18,10 @@
 import { Component, Prop, Vue } from "vue-property-decorator"
 import Card from "@/components/Card.vue"
 import Coin from "@/components/Coin.vue"
+import Playground from "@/components/Playground.vue"
 
 @Component({
-  components: { Card, Coin }
+  components: { Card, Coin, Playground }
 })
 export default class Board extends Vue {
 
@@ -30,7 +32,7 @@ export default class Board extends Vue {
 
 .hand{
   position: absolute;
-  bottom: 5%;
+  bottom: -5%;
   left:35%;
 
 }
@@ -43,5 +45,20 @@ export default class Board extends Vue {
 body {
   background-color: grey !important;
   background-image: none ;
+}
+
+.coin{
+  position: absolute;
+  bottom:8%;
+  left: 88%;
+}
+
+.coinSprite{
+  width: 100px;
+  height:100px;
+}
+
+.playground {
+  width: 75%;
 }
 </style>
